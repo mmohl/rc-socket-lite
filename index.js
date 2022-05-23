259ba97143697d785c6413c71d6b5b5effdbd3db
Maulana M Luthfy <maulana@gmd.id>
2022-05-23 17:37:05 +0700

    debug

--- index.js
+++ index.js
@@ -1,50 +1,18 @@
-const express = require('express');
-const app = express();
-const http = require('http');
-const server = http.createServer(app);
-const { Server } = require("socket.io");
-const io = new Server(server, { cors: { origin: '*' } });
+const app = require('express')();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-    return res.json({ message: 'hello' })
-})
-
-io.on('connection', (socket) => {
-    socket.on('create-room', payload => {
-        try {
-            payload = JSON.parse(payload)
-
-            const { roomName } = payload
-            socket.join(roomName)
-
-            socket.to(roomName).emit(`${roomName}-callback`, JSON.stringify({ status: 1, message: "success create room" }))
-        } catch (error) {
-            socket.to(roomName).emit(`${roomName}-callback`, JSON.stringify({ status: 0, message: "failed create room" }))
-        }
-    })
-
-    socket.on('leave-room', (payload) => {
-        try {
-            payload = JSON.parse(payload)
-
-            const { roomName } = payload
-            socket.leave(roomName)
-        } catch (error) {
-        }
-    })
-
-    socket.on('listen-room', (payload) => {
-        try {
-            payload = JSON.parse(payload)
-
-            const { roomName, data } = payload
-            socket.to(`${roomName}`).emit(data)
-        } catch (error) {
-            console.log(error)
-        }
-    })
+    res.json({ message: 'hai' })
 });
 
-server.listen(3000, () => {
-    console.log('listening on *:3000');
+io.on('connection', (socket) => {
+    socket.on('chat message', msg => {
+        io.emit('chat message', msg);
+    });
+});
+
+http.listen(port, () => {
+    console.log(`Socket.IO server running at http://localhost:${port}/`);
 });
