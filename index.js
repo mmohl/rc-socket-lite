072ed9af1b500da6353b844cee325190f0520de0
Maulana M Luthfy <maulana@gmd.id>
2022-05-23 17:40:39 +0700

    debug

--- index.js
+++ index.js
@@ -8,9 +8,39 @@ app.get('/', (req, res) => {
 });
 
 io.on('connection', (socket) => {
-    socket.on('chat message', msg => {
-        io.emit('chat message', msg);
-    });
+    socket.on('create-room', payload => {
+        try {
+            payload = JSON.parse(payload)
+
+            const { roomName } = payload
+            socket.join(roomName)
+
+            socket.to(roomName).emit(`${roomName}-callback`, JSON.stringify({ status: 1, message: "success create room" }))
+        } catch (error) {
+            socket.to(roomName).emit(`${roomName}-callback`, JSON.stringify({ status: 0, message: "failed create room" }))
+        }
+    })
+
+    socket.on('leave-room', (payload) => {
+        try {
+            payload = JSON.parse(payload)
+
+            const { roomName } = payload
+            socket.leave(roomName)
+        } catch (error) {
+        }
+    })
+
+    socket.on('listen-room', (payload) => {
+        try {
+            payload = JSON.parse(payload)
+
+            const { roomName, data } = payload
+            socket.to(`${roomName}`).emit(data)
+        } catch (error) {
+            console.log(error)
+        }
+    })
 });
 
 http.listen(port, () => {
