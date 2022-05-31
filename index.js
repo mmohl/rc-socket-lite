68f35fe89b97cd4997876caff62dc38339e395ec
Maulana M Luthfy <maulana@gmd.id>
2022-05-31 14:42:39 +0700

    debug

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.get('/', (req, res) => {
 });
 
 io.on('connection', (socket) => {
-    console.log(socket?.adapter)
+    console.log(socket?.adapter?.rooms)
     socket.on('make-room', payload => {
         console.log(payload)
         // payload = JSON.parse(payload)
